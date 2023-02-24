8b5e246e54fdc663be35a4128677cb3b8387237a
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-24 20:18:03 +0200

    Displayed results

--- RollADiceGame/javascript.js
+++ RollADiceGame/javascript.js
@@ -1,6 +1,6 @@
+window.onload = function() {
 var number_random_1 = Math.floor(Math.random()*6)+1;
 var number_random_2 = Math.floor(Math.random()*6)+1;
-
 
 var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
@@ -8,3 +8,12 @@ var player2 = document.getElementById("player2");
 player1.setAttribute('src', "dice-"+number_random_1+".png");
 player2.setAttribute('src', "dice-"+number_random_2+".png");
 
+var headerTitle = document.getElementById("headerTitle");
+if(number_random_1 > number_random_2){
+    headerTitle.innerHTML = 'Player 1 Wins';
+} else if(number_random_1 < number_random_2){
+    headerTitle.innerHTML = 'Player 2 Wins';
+} else{
+    headerTitle.innerHTML = 'It is a Draw';
+}
+}
