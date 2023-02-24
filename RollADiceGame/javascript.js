4a73f78dabff0b11eb3487d8fbe6e32e83759505
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-24 20:00:22 +0200

    Implemented changing dice to player 1

--- RollADiceGame/javascript.js
+++ RollADiceGame/javascript.js
@@ -1,5 +1,9 @@
 var number_random = Math.floor(Math.random()*6)+1;
+console.log(number_random);
 
 var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
+
+player1.setAttribute('src', "dice-"+number_random+".png");
+player2.setAttribute('src', "dice-"+number_random+".png");
 
